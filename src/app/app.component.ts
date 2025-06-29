2741e444069f47372ccbae67186f18ff6b873283
Jonathan Baldelomar <80008500+Jbaldmont@users.noreply.github.com>
2025-06-28 21:31:56 -0400

    [ic-2] install of primeng dependencies (#3)
    * update Angular dependencies to version 19.2.14
    
    * Add PrimeNG dependencies to package.json and package-lock.json
    
    * Add PrimeNG configuration and animations to appConfig
    
    * Add PrimeNG Toast and Button modules with message service to AppComponent
    
    * Remove placeholder content and add PrimeNG Toast and Button components to AppComponent
    
    ---------
    
    Co-authored-by: Jonathan Baldelomar <jonathan.baldelomar@endesyc.bo>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { Toast } from 'primeng/toast';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Toast, ButtonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: [MessageService],
 })
 export class AppComponent {
   title = 'icecube-app';
+
+  constructor(private messageService: MessageService) {}
+
+  public showToast() {
+    this.messageService.add({
+      severity: 'success',
+      summary: 'Toast Test',
+      detail: 'If you can see this message, that means toast works.',
+      life: 3000,
+    });
+  }
 }
